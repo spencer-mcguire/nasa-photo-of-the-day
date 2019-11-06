5f721df663eca50b6072a8b4ac3b013c7909b81c
spencer-mcguire <smcguire1826@gmail.com>
2019-11-06 15:53:09 -0600

    set structure

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<img
+				src='https://api.nasa.gov/assets/img/favicons/favicon-192.png'
+				alt='logo'
+			/>
+		</header>
+	);
+};
+export default Header;
